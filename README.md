8631d2b6e243154302fee579bf0e642ed854836d
EDMerstratec <erme0001@edu.mercantec.dk>
2023-11-10 11:12:10 +0100

    Hex0.8, comments and framework
    plan, now to execute

--- README.md
+++ README.md
@@ -13,25 +13,16 @@ catch invalid input values for binary
 DOCUMENTATION:
 
 ///Variables
-All have been moved out of global scope
-//block// is used to track which of four boxes is being modified
-//checkVal// is used in function scope to check for a valid value, otherwise adjusted to return invalid value message
-//num// takes text field value if check function succeeds
-//binaryNum// tracks string value during conversion loop, returns to binary text field at end
-//deciNum// tracks integer sum during conversion loop, returns to decimal text field at end
+All have been moved out of global scope  
+//block// is used to track which of four boxes is being modified  
+//checkVal// is used in function scope to check for a valid value, otherwise adjusted to return invalid value message  
+//num// takes text field value if check function succeeds  
+//binaryNum// tracks string value during conversion loop, returns to binary text field at end  
+//deciNum// tracks integer sum during conversion loop, returns to decimal text field at end  
 
-///Functions
-//checkDeci(block)// checks validity of decimal text field #block and runs convertDeci() or returns error message
-//convertDeci(num,block)// if check was valid, subtract powers of 2 (2^7 descending to 2^0) and add bits. Return full byte
+///Functions  
+//checkDeci(block)// checks validity of decimal text field #block and runs convertDeci() or returns error message  
+//convertDeci(num,block)// if check was valid, subtract powers of 2 (2^7 descending to 2^0) and add bits. Return full byte  
 
-function checkDeci(block) {
-    let checkVal = "ipDeci" + block;
-    checkVal = document.getElementById(checkVal).value
-    if (checkVal >= 0 && checkVal <= 255) {
-        convertDeci(checkVal,block);
-    }
-    else {
-        checkVal = "ipBinary" + block;
-        document.getElementById(checkVal).value = "NaN; 0-255 only"
-    }
-}
+///Hexadecimal expansion:  
+from 00 (or 0) to FF. base 16 = base 2^4? Looks like we can convert 4 bits into 1 hex.  
--- ipConverter.js
+++ ipConverter.js
@@ -1,36 +1,36 @@
-function checkDeci(block) {
-    let blockVal = "ipDeci" + block;
-    let checkVal = document.getElementById(blockVal).value
-    if (checkVal >= 0 && checkVal <= 255) {
+function checkDeci(block) {//block derived from input field
+    let blockVal = "ipDeci" + block; //full text field ID
+    let checkVal = document.getElementById(blockVal).value //actual input number
+    if (checkVal >= 0 && checkVal <= 255) { //if valid 0-255, convert function called
         convertDeci(checkVal,block);
     }
-    else {
+    else { //error message
         document.getElementById(blockVal).value = "NaN; 0-255 only"
     }
 }
 
 function convertDeci(num, block) { //number to convert, block to insert
-    let binaryNum = ""; //reset
+    let binaryNum = ""; //establish empty string to fill
     for (let i = 7; i >= 0; i--) { //8 bits, 8 iterations
         if (num >= (2**i)) { //check against 128, 64, etc. 2**i
-            num -= (2**i)
+            num -= (2**i) //if larger, subtract from num and append 1 to string
             binaryNum += "1";
         }
-        else {
+        else { //not activated bit
             binaryNum += "0";
         }
     }
-    block = "ipBinary" + block
+    block = "ipBinary" + block //assign to correct text field
     document.getElementById(block).value = binaryNum //return new binary
 }
 
-function checkBinary(block){
-    let blockVal = "ipBinary" + block
-    checkVal = document.getElementById(blockVal).value
-    if (checkVal.length === 8 && /^[0-1]*$/.test(checkVal)) {
-        convertBinary(checkVal,block);
+function checkBinary(block){ //block set from input field
+    let blockVal = "ipBinary" + block //full ID 
+    let checkVal = document.getElementById(blockVal).value //actual input via ID
+    if (checkVal.length === 8 && /^[0-1]*$/.test(checkVal)) { //octet of 1s and 0s only
+        convertBinary(checkVal,block); //call conversion function
     }
-    else {
+    else { //input error message
         document.getElementById(blockVal).value = "8 length binary only"
     }
 }
@@ -38,8 +38,25 @@ function checkBinary(block){
 function convertBinary(binaryVal,block) {
     let blockVal = "ipDeci" + block;
     let deciSum = 0
-    for (let i = 0; i < 8; i++) {
-        deciSum += (binaryVal[i] * (2**(7 - i)));
+    for (let i = 0; i < 8; i++) {//iterate over each bit
+        deciSum += (binaryVal[i] * (2**(7 - i))); //if bit isn't 0, multiply power
     }
-    document.getElementById(blockVal).value = deciSum
+    document.getElementById(blockVal).value = deciSum //insert new decimal to field
 }
+
+function checkHex(block) {
+    let blockVal = "ipHex" + block;
+    let checkVal = document.getElementById(blockVal).value;
+    if (checkVal.length <= 2 && /^[0-9A-F]*$/.test(checkVal)) {
+        convertHex(checkVal,block)
+    }
+    else {
+        document.getElementById(blockVal).value = "Hex 00 to FF only"
+    }
+}
+
+function convertHex(num,block) {
+    
+}
+
+//plan: 8 bit: 0-15 correspond to second 4 bits, 0-15*16 to first
